d28eef1fa662003631837574319e7b75456f9543
WiiruxCruz <hialf.92@gmail.com>
2023-01-25 00:19:17 -0600

    initial commit

---
+++ src/main/java/com/wiirux/sdjpa/wp/SdjpaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.wiirux.sdjpa.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiirux/sdjpa/wp/SdjpaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiirux.sdjpa.wp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
